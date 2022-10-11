6d81ec28af84af5c9b278e6fdc409b3a3db39610
Clonazepunk <facundoaguirre1989@gmail.com>
2022-10-11 18:27:36 -0300

    sobre nosotros

--- src/components/sobreNosotros/sobreNosotros.js
+++ src/components/sobreNosotros/sobreNosotros.js
@@ -1,11 +1,128 @@
-import React from 'react'
+import React from "react";
+import "./sobreNosotros.css";
+import linkedin from '../../ilustraciones/nosotros/staff/linkedin.png';
+import staff1 from '../../ilustraciones/nosotros/staff/staff1.png';
+import staff2 from '../../ilustraciones/nosotros/staff/staff2.png';
+import staff3 from '../../ilustraciones/nosotros/staff/staff3.png';
 
 const sobreNosotros = () => {
-  return (
-    <div>
-      xdxdxdxd
-    </div>
-  )
-}
+  const staff = [
+    {
+      foto: staff1,
+      nombre: "Rosmarie Bahrens",
+      bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+      puesto: "Graphic Designer",
+      linkedin: "#",
+    },
+    {
+        foto: staff2,
+      nombre: "Jun Wang",
+      bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+      puesto: "Marketing",
+      linkedin: "#",
+    },
+    {
+        foto: staff3,
+      nombre: "Juan José García",
+      bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+      puesto: "CEO",
+      linkedin: "#",
+    },
+    {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
+      {
+        foto: staff3,
+        nombre: "NPC",
+        bio: "asklñdas ilnajdjlk fjnifalopagjkldfkjlgdf klgpeo gmdflk mbdflkdfjbdlfmbdflm dfkmdflkdfm vdk opv",
+        puesto: "Random",
+        linkedin: "#",
+      },
 
-export default sobreNosotros
+  ];
+
+  return (
+    <div className="contenedor">
+      {staff.map((e) => (
+        <div className="tarjeta">
+          <img src={e.foto} className="profile-pic"/>
+          <a src={e.linkedin} className="linkedin">
+            <img src={linkedin}></img>
+          </a>
+          <h2>{e.nombre}</h2>
+          <p>"{e.bio}"</p>
+          <div className="button">{e.puesto}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default sobreNosotros;
--- src/pages/more/MoreUs.js
+++ src/pages/more/MoreUs.js
@@ -1,33 +1,52 @@
 import React from "react";
-import sobreNosotros from "../../components/sobreNosotros/sobreNosotros";
+import SobreNosotros from "../../components/sobreNosotros/SobreNosotros.js";
 import "./MoreUs.css";
+import nosotros1 from "../../ilustraciones/nosotros/nosotros1.png";
+import user1 from "../../ilustraciones/nosotros/1.png";
+import user2 from "../../ilustraciones/nosotros/2.png";
+import user3 from "../../ilustraciones/nosotros/3.png";
+import user4 from "../../ilustraciones/nosotros/4.png";
+
 const MoreUs = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <h1>
+    <div className="">
+      <div className="conocenos justify-center mt-10 items-center back-grad pt-10 pb-10">
+        <div className="border h-2/3">
+          <h1 className=" title pl-10 pr-10 font-bold c-azul leading-snug">
             ¿Te gustaría ser parte del equipo Pinflag?{" "}
             <span className="c-calipso">¡Conócenos!</span>
           </h1>
-          <div className="dotted-img"></div>
-          <div className="dotted-img"></div>
+
+          <div className="conocenos-left py-10 ">
+            <img src={user1} className="user user1"  alt="" />
+            <img src={user2} className="user user2"  alt="" />
+          </div>
         </div>
-        <div>
-          <div className="dotted-img"></div>
-          <div className="dotted-img"></div>
-          <div className="dotted-img"></div>
+
+        <div className="border w-full conocenos-right justify-around h-2/3">
+          <img src={user3} className="user  user3"  alt="" />
+          <img src={user4} className="user  user4" alt="" />
+
+          <div className="flex flex-col justify-center  user5" >
+            <div className="dotted-img self-start "></div>
+            <p className="c-azul">¡Aquí podrías estar tú!</p>
+          </div>
         </div>
       </div>
-      <h2>
+      <h2 className="text-center font-black  mt-20 mb-20 c-azul subtitle">
         Sobre <span className="c-calipso">nosotros</span>
       </h2>
-      <div>
-
+      <SobreNosotros />
+      <div className="flex justify-center">
+        <img src={nosotros1} class="nosotros-1 mt-20" alt="" />
       </div>
-      <div>
-        <h2>¡Te estamos buscando!</h2>
-        <button>Postula aquí</button>
+      <div className="bg-violet text-center">
+        <h2 className="text-white  font-semi-bold pt-20 mb-5 subtitle">
+          ¡Te estamos buscando!
+        </h2>
+        <button className="py-4 px-10 rounded-full text-white font-bold mb-20">
+          Postula aquí
+        </button>
       </div>
     </div>
   );
